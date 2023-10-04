ba46c173861f629cdc9892265cd5a15c8fb332b5
Taomero <82759172+Taomero@users.noreply.github.com>
2023-10-04 20:10:46 +0200

    Update Main.java

--- HelloGit/untitled/src/main/java/org/example/Main.java
+++ HelloGit/untitled/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git novoooo");
         System.out.println("HIII");
         user user = new user(14, "Sebastian");
     }
