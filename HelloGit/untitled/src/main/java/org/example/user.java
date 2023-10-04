5ef179cb5e1cf7d8833e3bd0a9a4309518516698
taomero <tahir_007@live.com>
2023-10-04 20:07:12 +0200

    unchecked

---
+++ HelloGit/untitled/src/main/java/org/example/user.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class user {
+    private int num;
+    private String name;
+
+    public user(int num, String name)
+    {
+        num=this.num;
+        name=this.name;
+
+    }
+}
