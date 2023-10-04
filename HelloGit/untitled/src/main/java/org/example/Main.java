aecc9a7cbe31195e5992c5ce26b6afc604544c30
taomero <tahir_007@live.com>
2023-10-04 20:06:29 +0200

    all

--- HelloGit/untitled/src/main/java/org/example/Main.java
+++ HelloGit/untitled/src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        System.out.println("Hello GIT");
+        System.out.println("Hello Git");
+        System.out.println("HIII");
+        user user = new user(14, "Sebastian");
     }
-     String user = new user(15, "DR").toString();
+
 }
